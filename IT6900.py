504d256531082d5448d45f6655ca119a5f0877eb
Andrey Sanin <sanin_andrey@mail.ru>
2022-05-04 21:34:02 +0700

    Initial commit

---
+++ IT6900.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import sys
+import serial
+
+sys.path.append('../TangoUtils')
+from Moxa import MoxaTCPComPort
+from config_logger import config_logger
+from log_exception import log_exception
+
+LF = b'\n'
+DEVICE_NAME = 'IT6900'
+DEVICE_FAMILY = 'IT6900 family Power Supply'
+ID_OK = 'ITECH Ltd., IT69'
+MIN_TIMEOUT = 0.1
+READ_TIMEOUT = 0.5
+
+
+class IT6900Exception(Exception):
+    pass
+
+
+class IT6900:
+
+    def __init__(self, port: str, *args, **kwargs):
+        # configure logger
+        if 'logger' not in kwargs or kwargs['logger'] is None:
+            self.logger = config_logger()
+        else:
+            self.logger = kwargs.pop('logger')
+        # parameters
+        self.io_count = 0
+        self.io_error_count = 0
+        self.avg_io_time = 0.0
+        self.max_io_time = 0.0
+        self.min_io_time = 1000.0
+        #
+        self.port = port.strip()
+        self.args = args
+        self.kwargs = kwargs
+        # create variables
+        self.command = b''
+        self.response = b''
+        # timeouts
+        self.timeout_time = float('inf')
+        # default com port, id, and serial number
+        self.com = None
+        self.id = 'Unknown Device'
+        self.type = 'Unknown Device'
+        self.sn = ''
+        self.max_voltage = float('inf')
+        self.max_current = float('inf')
+        self.ready = False
+        # create and open COM port
+        self.com = self.create_com_port()
+        if self.com is None:
+            self.logger.error('Can not open serial port')
+            self.ready = False
+            return
+        # further initialization (for possible async use)
+        self.init()
+
+    def create_com_port(self):
+        try:
+            if (self.port.upper().startswith('COM')
+                    or self.port.startswith('tty')
+                    or self.port.startswith('/dev')
+                    or self.port.startswith('cua')):
+                if 'timeout' not in self.kwargs:
+                    self.kwargs['timeout'] = 0.0
+                # COM port will be openet automatically after creation
+                self.com = serial.Serial(self.port, *self.args, **self.kwargs)
+            else:
+                self.com = MoxaTCPComPort(self.port, *self.args, **self.kwargs)
+            if self.com.isOpen():
+                self.logger.debug('Port %s is ready', self.port)
+                return self.com
+        except:
+            log_exception(self)
+        self.logger.error('COM port creation error')
+        self.com = None
+        return self.com
+
+    def init(self):
+        # device id, sn and type
+        self.id = self.read_device_id()
+        if not self.id.startswith(ID_OK):
+            self.ready = False
+            self.logger.error('%s initialization error', DEVICE_NAME)
+            return
+        self.ready = True
+        self.sn = self.read_serial_number()
+        self.type = self.read_device_type()
+        # switch to remote mode
+        self.switch_remote()
+        self.clear_status()
+        # read maximal voltage and current
+        if self.send_command('VOLT? MAX'):
+            self.max_voltage = float(self.response[:-1])
+        if self.send_command('CURR? MAX'):
+            self.max_current = float(self.response[:-1])
+        msg = 'Device has been initialized %s' % self.id
+        self.logger.debug(msg)
+
+    def send_command(self, cmd, check_response=None):
+        self.io_count += 1
+        try:
+            # unify command
+            cmd = cmd.upper().strip()
+            # convert str to bytes
+            if isinstance(cmd, str):
+                cmd = str.encode(cmd)
+            if not cmd.endswith(LF):
+                cmd += LF
+            self.response = b''
+            t0 = time.perf_counter()
+            # write command
+            if not self.write(cmd):
+                return False
+            if check_response is None:
+                if b'?' in cmd:
+                    check_response = True
+                else:
+                    check_response = False
+            if not check_response:
+                return True
+            # read response (to LF by default)
+            result = self.read_response()
+            # reding time stats
+            dt = time.perf_counter() - t0
+            self.min_io_time = min(self.min_io_time, dt)
+            self.max_io_time = max(self.max_io_time, dt)
+            self.avg_io_time = (self.avg_io_time * (self.io_count - 1) + dt) / self.io_count
+            if not result:
+                self.io_error_count += 1
+            self.logger.debug('%s -> %s, %s, %4.0f ms', cmd, self.response, result, dt * 1000)
+            return result
+        except:
+            self.io_error_count += 1
+            log_exception(self, 'Command %s exception', cmd)
+            return False
+
+    @property
+    def timeout(self):
+        if time.perf_counter() > self.timeout_time:
+            return True
+        return False
+
+    @timeout.setter
+    def timeout(self, value):
+        if value is not None:
+            self.timeout_time = time.perf_counter() + value
+        else:
+            self.timeout_time = float('inf')
+
+    def read(self, size=1, timeout=None):
+        result = b''
+        self.timeout = timeout
+        try:
+            while len(result) < size:
+                if self.com.in_waiting > 0:
+                    r = self.com.read(1)
+                    if len(r) > 0:
+                        result += r
+                else:
+                    if self.timeout:
+                        self.logger.error('Reading timeout')
+                        return result
+        except:
+            log_exception(self)
+        return result
+
+    def read_until(self, terminator=LF, size=None, timeout=READ_TIMEOUT):
+        result = b''
+        r = b''
+        while terminator not in r:
+            r = self.read(1, timeout=timeout)
+            if len(r) <= 0:
+                return result
+            result += r
+            if size is not None and len(result) >= size:
+                return result
+        return result
+
+    def read_response(self, expected=LF):
+        result = self.read_until(expected)
+        self.response = result
+        if expected not in result:
+            self.logger.error('Response %s without %s ', result, expected)
+            return False
+        return True
+
+    def write(self, cmd):
+        # t0 = time.perf_counter()
+        try:
+            # reset buffers
+            self.com.reset_input_buffer()
+            self.com.reset_output_buffer()
+            # write command
+            length = self.com.write(cmd)
+            if len(cmd) != length:
+                self.logger.error('Write error %s of %s' % (length, len(cmd)))
+                return False
+            # dt = (time.perf_counter() - t0) * 1000.0
+            # self.logger.debug('%s %s bytes in %4.0f ms', cmd, length, dt)
+            return True
+        except:
+            log_exception(self, 'Exception during write')
+            return False
+
+    def read_value(self, cmd, v_type=float):
+        try:
+            if self.send_command(cmd):
+                return v_type(self.response)
+            else:
+                return None
+        except:
+            self.logger.debug('Can not convert %s to %s', self.response, v_type)
+            return None
+
+    def write_value(self, cmd, value):
+        if isinstance(cmd, str):
+            cmd = cmd.encode()
+        cmd1 = cmd.upper().strip()
+        cmd2 = cmd1 + b' ' + str(value).encode() + b';' + cmd1 + b'?'
+        v = self.read_value(cmd2, type(value))
+        return value == v
+
+    def read_output(self):
+        if not self.send_command(b'OUTP?'):
+            return None
+        response = self.response.upper()
+        if response.startswith((b'ON', b'1')):
+            return True
+        if response.startswith((b'OFF', b'0')):
+            return False
+        self.logger.info('Unexpected response %s' % response)
+        return None
+
+    def write_output(self, value: bool):
+        if value:
+            t_value = 'ON'
+        else:
+            t_value = 'OFF'
+        self.write_value(b'OUTP', t_value)
+        return bool(self.response[:-1]) == value
+
+    def write_voltage(self, value: float):
+        return self.write_value(b'VOLT', value)
+
+    def write_current(self, value: float):
+        return self.write_value(b'CURR', value)
+
+    def read_current(self):
+        return self.read_value(b'MEAS:CURR?')
+
+    def read_programmed_current(self):
+        return self.read_value(b'CURR?')
+
+    def read_voltage(self):
+        return self.read_value(b'MEAS:VOLT?')
+
+    def read_programmed_voltage(self):
+        return self.read_value(b'VOLT?')
+
+    def read_power(self):
+        return self.read_value(b'MEAS:POW?')
+
+    def read_device_id(self):
+        try:
+            if self.send_command(b':*IDN?'):
+                return self.response[:-1].decode()
+            else:
+                return 'Unknown Device'
+        except:
+            return 'Unknown Device'
+
+    def read_serial_number(self):
+        try:
+            if self.send_command(b'*IDN?'):
+                serial_number = self.response[:-1].decode().split(',')[2]
+                return serial_number
+            else:
+                return ""
+        except:
+            return ""
+
+    def read_device_type(self):
+        try:
+            if self.send_command(b'*IDN?'):
+                return self.response[:-1].decode().split(',')[1]
+            else:
+                return "Unknown Device"
+        except:
+            return "Unknown Device"
+
+    def close_com_port(self):
+        self.ready = False
+        try:
+            self.com.close()
+        except:
+            pass
+
+    def switch_remote(self):
+        return self.send_command(b'SYST:REM', False)
+
+    def read_errors(self):
+        if self.send_command(b'SYST:ERR?'):
+            return self.response[:-1].decode()
+        else:
+            return ''
+
+    def switch_local(self):
+        return self.send_command(b'SYST:LOC', False)
+
+    def clear_status(self):
+        return self.send_command(b'*CLS', False)
+
+    def reconnect(self, port=None, *args, **kwargs):
+        if port is not None:
+            self.port = port.strip()
+        if len(args) > 0:
+            self.args = args
+        if len(kwargs) > 0:
+            self.kwargs = kwargs
+        self.ready = False
+        self.close_com_port()
+        self.com = self.create_com_port()
+        # self.com.reset_output_buffer()
+        # self.com.reset_input_buffer()
+        # self.send_command('*IDN?', False)
+        self.init()
+        # print(self.read_errors())
+
+    def initialized(self):
+        return self.ready
+
+    def detect_baud(self):
+        if self.ready:
+            return
+        bauds = (115200, 9600, 4800, 19200, 38400, 57600)
+        #bauds = (115200,)
+        for baud in bauds:
+            self.logger.debug('Try reconnect at %s', baud)
+            self.kwargs['baudrate'] = baud
+            time.sleep(2.0)
+            self.reconnect()
+            if self.ready:
+                self.logger.debug('Reconnected successfully at %s', baud)
+                return
+
+
+if __name__ == "__main__":
+    pd1 = IT6900("COM3", baudrate=115200)
+    # pd1.detect_baud()
+    for i in range(100):
+        cmd = ":*IDN?"
+        t_0 = time.time()
+        v1 = pd1.send_command(cmd)
+        dt1 = int((time.time() - t_0) * 1000.0)  # ms
+        print(pd1.port, cmd, '->', pd1.response, v1, '%4d ms ' % dt1)
+        cmd = "VOLT?"
+        t_0 = time.time()
+        v1 = pd1.send_command(cmd)
+        dt1 = int((time.time() - t_0) * 1000.0)  # ms
+        print(pd1.port, cmd, '->', pd1.response, v1, '%4d ms ' % dt1)
+    print('Total I/O:', pd1.io_count)
+    print('Total Errors:', pd1.io_error_count)
+    print('min I/O time:', pd1.min_io_time)
+    print('max I/O time:', pd1.max_io_time)
+    print('avg I/O time:', pd1.avg_io_time)
---
+++ IT6900_Server.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""IT6900 family power supply tango device server"""
+import sys
+import threading
+import time
+from math import isnan
+
+import tango
+from tango import AttrQuality, AttrWriteType, DispLevel
+from tango import DevState
+from tango.server import Device, attribute, command
+
+import IT6900
+from config_logger import config_logger
+
+sys.path.append('../TangoUtils')
+from TangoServerPrototype import TangoServerPrototype
+
+ORGANIZATION_NAME = 'BINP'
+APPLICATION_NAME = 'IT6900 family Power Supply Tango Device Server'
+APPLICATION_NAME_SHORT = 'IT6900_Server'
+APPLICATION_VERSION = '1.0'
+
+
+class IT6900_Server(TangoServerPrototype):
+    server_version = APPLICATION_VERSION
+    server_name = APPLICATION_NAME
+
+    port = attribute(label="Port", dtype=str,
+                     display_level=DispLevel.OPERATOR,
+                     access=AttrWriteType.READ,
+                     unit="", format="%s",
+                     doc="TDKLambda port")
+
+    device_type = attribute(label="PS Type", dtype=str,
+                            display_level=DispLevel.OPERATOR,
+                            access=AttrWriteType.READ,
+                            unit="", format="%s",
+                            doc="TDKLambda device type")
+
+    output_state = attribute(label="Output", dtype=bool,
+                             display_level=DispLevel.OPERATOR,
+                             access=AttrWriteType.READ_WRITE,
+                             unit="", format="",
+                             doc="Output on/off state")
+
+    voltage = attribute(label="Voltage", dtype=float,
+                        display_level=DispLevel.OPERATOR,
+                        access=AttrWriteType.READ,
+                        unit="V", format="%6.3f",
+                        min_value=0.0,
+                        doc="Measured voltage")
+
+    programmed_voltage = attribute(label="Programmed Voltage", dtype=float,
+                                   display_level=DispLevel.OPERATOR,
+                                   access=AttrWriteType.READ_WRITE,
+                                   unit="V", format="%6.3f",
+                                   min_value=0.0,
+                                   doc="Programmed voltage")
+
+    current = attribute(label="Current", dtype=float,
+                        display_level=DispLevel.OPERATOR,
+                        access=AttrWriteType.READ,
+                        unit="A", format="%6.3f",
+                        min_value=0.0,
+                        doc="Measured current")
+
+    programmed_current = attribute(label="Programmed Current", dtype=float,
+                                   display_level=DispLevel.OPERATOR,
+                                   access=AttrWriteType.READ_WRITE,
+                                   unit="A", format="%6.3f",
+                                   min_value=0.0,
+                                   doc="Programmed current")
+
+    power = attribute(label="Power", dtype=float,
+                      display_level=DispLevel.OPERATOR,
+                      access=AttrWriteType.READ,
+                      unit="W", format="%8.3f",
+                      min_value=0.0,
+                      doc="Measured output power")
+
+    def init_device(self):
+        super().init_device()
+        if self not in IT6900_Server.device_list:
+            IT6900_Server.device_list.append(self)
+        kwargs = {}
+        args = ()
+        port = self.config.get('port', 'COM3')
+        baud = self.config.get('baudrate', 115200)
+        kwargs['baudrate'] = baud
+        kwargs['logger'] = self.logger
+        self.it6900 = IT6900.IT6900(port, *args, **kwargs)
+        if self.it6900.initialized():
+            # add device to list
+            self.programmed_voltage.set_max_value(self.it6900.max_voltage)
+            self.programmed_current.set_max_value(self.it6900.max_current)
+            self.programmed_voltage.set_write_value(self.read_programmed_voltage())
+            self.programmed_current.set_write_value(self.read_programmed_current())
+            self.output_state.set_write_value(self.read_output_state())
+            # set state to running
+            self.set_state(DevState.RUNNING)
+            self.set_status('Successfully initialized')
+            msg = '%s at %s initialized successfully' % (self.it6900.type, self.it6900.port)
+            self.logger.info(msg)
+        else:
+            self.logger.error('Device initialization error')
+            self.set_state(DevState.FAULT)
+            self.set_status('Initialization error')
+
+    def delete_device(self):
+        self.it6900.ready = False
+        self.it6900.close_com_port()
+        if self in IT6900_Server.devices:
+            IT6900_Server.devices.remove(self)
+        self.logger.info('Device has been deleted')
+
+    def read_port(self):
+        if self.it6900.initialized():
+            return self.it6900.port
+        return "Uninitialized"
+
+    def read_device_type(self):
+        if self.it6900.initialized():
+            return self.it6900.type
+        return "Uninitialized"
+
+    def read_output_state(self):
+        if self.it6900.initialized():
+            value = self.it6900.read_output()
+            if value is not None:
+                qual = AttrQuality.ATTR_VALID
+                self.set_running()
+            else:
+                qual = AttrQuality.ATTR_INVALID
+                value = False
+                self.set_fault()
+        else:
+            value = False
+            qual = AttrQuality.ATTR_INVALID
+            self.set_fault('I/O to uninitialized device')
+        self.output_state.set_value(value)
+        self.output_state.set_quality(qual)
+        return value
+
+    def common_read(self, read_function, attrib, wvalue=None):
+        if not self.it6900.initialized():
+            attrib.set_value(wvalue)
+            attrib.set_quality(AttrQuality.ATTR_INVALID)
+            self.set_fault('Read from uninitialized device')
+            return wvalue
+        value = read_function()
+        if value is not None:
+            attrib.set_value(value)
+            attrib.set_quality(AttrQuality.ATTR_VALID)
+            self.set_running()
+            return value
+        else:
+            attrib.set_value(wvalue)
+            attrib.set_quality(AttrQuality.ATTR_INVALID)
+            self.set_fault()
+            return wvalue
+
+    def common_write(self, write_function, attrib, value):
+        if not self.it6900.initialized():
+            attrib.set_quality(AttrQuality.ATTR_INVALID)
+            msg = "Write to offline device %s" % self.name
+            self.logger.warning(msg)
+            self.set_fault(msg)
+            return False
+        if write_function(value):
+            attrib.set_quality(AttrQuality.ATTR_VALID)
+            self.set_running()
+            return True
+        attrib.set_quality(AttrQuality.ATTR_INVALID)
+        self.set_fault()
+        return False
+
+    def write_output_state(self, value):
+        return self.common_write(self.it6900.write_output, self.output_state, value)
+        # if not self.it6900.initialized():
+        #     self.output_state.set_quality(AttrQuality.ATTR_INVALID)
+        #     msg = "Write to offline device %s" % self.name
+        #     self.logger.warning(msg)
+        #     self.set_fault(msg)
+        #     return False
+        # else:
+        #     if self.it6900.write_output(value):
+        #         self.output_state.set_quality(AttrQuality.ATTR_VALID)
+        #         result = True
+        #         self.set_running()
+        #     else:
+        #         self.output_state.set_quality(AttrQuality.ATTR_INVALID)
+        #         result = False
+        #         self.set_fault()
+        # return result
+
+    def read_power(self):
+        return self.common_read(self.it6900.read_power, self.power)
+
+    def read_voltage(self):
+        if self.it6900.initialized():
+            value = self.it6900.read_voltage()
+            if value is not None:
+                qual = AttrQuality.ATTR_VALID
+                self.set_running()
+            else:
+                qual = AttrQuality.ATTR_INVALID
+                value = float('nan')
+                self.set_fault()
+        else:
+            value = float('nan')
+            qual = AttrQuality.ATTR_INVALID
+            self.set_fault('I/O to uninitialized device')
+        self.voltage.set_value(value)
+        self.voltage.set_quality(qual)
+        return value
+
+    def read_current(self):
+        if self.it6900.initialized():
+            value = self.it6900.read_voltage()
+            if value is not None:
+                qual = AttrQuality.ATTR_VALID
+                self.set_running()
+            else:
+                qual = AttrQuality.ATTR_INVALID
+                value = float('nan')
+                self.set_fault()
+        else:
+            value = float('nan')
+            qual = AttrQuality.ATTR_INVALID
+            self.set_fault('I/O to uninitialized device')
+        self.current.set_value(value)
+        self.current.set_quality(qual)
+        return value
+
+    def read_programmed_voltage(self):
+        if self.it6900.initialized():
+            value = self.it6900.read_programmed_voltage()
+            if value is not None:
+                qual = AttrQuality.ATTR_VALID
+                self.set_running()
+            else:
+                qual = AttrQuality.ATTR_INVALID
+                value = float('nan')
+                self.set_fault()
+        else:
+            value = float('nan')
+            qual = AttrQuality.ATTR_INVALID
+            self.set_fault('I/O to uninitialized device')
+        self.programmed_voltage.set_value(value)
+        self.programmed_voltage.set_quality(qual)
+        return value
+
+    def read_programmed_current(self):
+        if self.it6900.initialized():
+            value = self.it6900.read_programmed_current()
+            if value is not None:
+                qual = AttrQuality.ATTR_VALID
+                self.set_running()
+            else:
+                qual = AttrQuality.ATTR_INVALID
+                value = float('nan')
+                self.set_fault()
+        else:
+            value = float('nan')
+            qual = AttrQuality.ATTR_INVALID
+            self.set_fault('I/O to uninitialized device')
+        self.programmed_current.set_value(value)
+        self.programmed_current.set_quality(qual)
+        return value
+
+    def write_programmed_voltage(self, value):
+        if not self.it6900.initialized():
+            msg = "Writing to offline device %s" % self.name
+            self.logger.warning(msg)
+            self.set_fault(msg)
+            return False
+        else:
+            result = self.it6900.write_voltage(value)
+        if result:
+            self.programmed_voltage.set_quality(AttrQuality.ATTR_VALID)
+            self.set_running()
+        else:
+            self.programmed_voltage.set_quality(AttrQuality.ATTR_INVALID)
+            self.set_fault()
+        return result
+
+    def write_programmed_current(self, value):
+        if not self.it6900.initialized():
+            self.programmed_voltage.set_quality(AttrQuality.ATTR_INVALID)
+            msg = "Writing to offline device %s" % self.name
+            self.logger.warning(msg)
+            self.set_fault(msg)
+            return False
+        else:
+            result = self.it6900.write_current(value)
+        if result:
+            self.programmed_voltage.set_quality(AttrQuality.ATTR_VALID)
+            self.set_running()
+        else:
+            self.programmed_voltage.set_quality(AttrQuality.ATTR_INVALID)
+            self.set_fault()
+        return result
+
+    @command
+    def reconnect(self):
+        self.it6900.reconnect()
+        self.it6900.switch_remote()
+
+    @command
+    def switch_remote(self):
+        self.it6900.switch_remote()
+
+    @command
+    def clear_status(self):
+        self.it6900.clear_status()
+
+    @command(dtype_in=str, doc_in='Directly send command to the device',
+             dtype_out=str, doc_out='Response from device without final LF')
+    def send_command(self, cmd):
+        if self.it6900.send_command(cmd):
+            self.set_running('Command Ok')
+        else:
+            self.set_fault('Command Error')
+        return self.it6900.response[:-1].decode()
+
+
+def looping():
+    time.sleep(1.0)
+    for dev in IT6900_Server.device_list:
+        dev.logger.debug("Test loop")
+
+
+if __name__ == "__main__":
+    # IT6900_Server.run_server(event_loop=looping)
+    IT6900_Server.run_server()
